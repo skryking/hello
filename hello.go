0c758dc3f5d4ce654bebc8585c336b5188ac8270
Jason Ormes <skryking@gmail.com>
2015-07-12 02:36:29 +0000

    using a library

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/skryking/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
+
